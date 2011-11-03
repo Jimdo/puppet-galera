6abfdeb798a28b5a90f387bf901efef151570344
Lars Fronius <lars@jimdo.com>
2011-11-03 11:36:38 +0100

    Added port to wsrep.cnf.erb and deleted apt-conf

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,10 +10,4 @@ Exec {
 
 exec { "apt-get-update" :
     command => "/usr/bin/apt-get update",
-    require => File["/etc/apt/apt.conf.d/01proxy"]
-}
-
-file { "/etc/apt/apt.conf.d/01proxy":
-    ensure => "file",
-    source => "/tmp/vagrant-puppet/manifests/files/apt-conf"
 }
