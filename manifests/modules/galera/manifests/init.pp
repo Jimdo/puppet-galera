63ebe3f813ddeb0860d5688306dca04b8f950561
Lars Fronius <lars@jimdo.com>
2011-11-03 15:18:40 +0100

    Master and Slave need mysql-user wsrep_sst. I also "retabbed" the init.pp

--- manifests/modules/galera/manifests/init.pp
+++ manifests/modules/galera/manifests/init.pp
@@ -4,59 +4,57 @@ class galera($cluster_name, $master_ip = false) {
     $mysql_password = "password"
 
     service { "mysql-galera" :
-        ensure => "running",
-        name => "mysql",
-        require => Package["mysql-server-wsrep","galera"],
-        hasrestart => true,
-        subscribe => File["/etc/mysql/my.cnf","/etc/mysql/conf.d/wsrep.cnf"],
+        ensure      => "running",
+        name        => "mysql",
+        require     => Package["mysql-server-wsrep","galera"],
+        hasrestart  => true,
+        subscribe   => File["/etc/mysql/my.cnf","/etc/mysql/conf.d/wsrep.cnf"],
     }
 
-    package { [ "mysql-client-5.1" ] :
-        ensure => present,
+    package { "mysql-client-5.1" :
+        ensure      => present,
     }
 
     package { "mysql-server-wsrep" :
-        ensure   => present,
-        provider => "dpkg",
-        source   => "/tmp/mysql-server-wsrep-5.1.58-21.1-amd64.deb",
-        require  => [Exec["download-wsrep"],Package["mysql-client-5.1"]],
+        ensure      => present,
+        provider    => "dpkg",
+        source      => "/tmp/mysql-server-wsrep-5.1.58-21.1-amd64.deb",
+        require     => [Exec["download-wsrep"],Package["mysql-client-5.1"]],
     }
 
     exec { "download-wsrep" :
-        command => "wget -O /tmp/mysql-server-wsrep-5.1.58-21.1-amd64.deb http://launchpad.net/codership-mysql/5.1/5.1.58-21.1/+download/mysql-server-wsrep-5.1.58-21.1-amd64.deb",
-        creates => "/tmp/mysql-server-wsrep-5.1.58-21.1-amd64.deb"
+        command     => "wget -O /tmp/mysql-server-wsrep-5.1.58-21.1-amd64.deb http://launchpad.net/codership-mysql/5.1/5.1.58-21.1/+download/mysql-server-wsrep-5.1.58-21.1-amd64.deb",
+        creates     => "/tmp/mysql-server-wsrep-5.1.58-21.1-amd64.deb"
     }
 
     package { "galera" :
-        ensure   => present,
-        provider => "dpkg",
-        source   => "/tmp/galera-21.1.0-amd64.deb",
-        require  => [Exec["download-galera"],Package["mysql-client-5.1"]],
+        ensure      => present,
+        provider    => "dpkg",
+        source      => "/tmp/galera-21.1.0-amd64.deb",
+        require     => [Exec["download-galera"],Package["mysql-client-5.1"]],
     }
 
     exec { "download-galera" :
-        command => "wget -O /tmp/galera-21.1.0-amd64.deb http://launchpad.net/galera/1.x/1.0/+download/galera-21.1.0-amd64.deb",
-        creates => "/tmp/galera-21.1.0-amd64.deb",
+        command     => "wget -O /tmp/galera-21.1.0-amd64.deb http://launchpad.net/galera/1.x/1.0/+download/galera-21.1.0-amd64.deb",
+        creates     => "/tmp/galera-21.1.0-amd64.deb",
     }
 
     file { "/etc/mysql/conf.d/wsrep.cnf" :
-        ensure => present,
-        content => template("galera/wsrep.cnf.erb"),
-        require => Package["mysql-server-wsrep","galera"],
+        ensure      => present,
+        content     => template("galera/wsrep.cnf.erb"),
+        require     => Package["mysql-server-wsrep","galera"],
     }
     
     file { "/etc/mysql/my.cnf" :
-        ensure => present,
-        content => template("galera/my.cnf.erb"),
-        require => Package["mysql-server-wsrep","galera"],
+        ensure      => present,
+        content     => template("galera/my.cnf.erb"),
+        require     => Package["mysql-server-wsrep","galera"],
     }
-    if ( $master_ip == false ) {
-        exec { "set-mysql-password":
-            unless => "/usr/bin/mysql -u${mysql_user} -p${mysql_password}",
-            command => "/usr/bin/mysql -uroot -e \"set wsrep_on='off'; delete from mysql.user where user=''; grant all on *.* to '${mysql_user}'@'%' identified by '${mysql_password}';\"",
-            require => Service["mysql-galera"],
-            subscribe => Service["mysql-galera"],
-            refreshonly => true,
-        }
+    exec { "set-mysql-password":
+        unless      => "/usr/bin/mysql -u${mysql_user} -p${mysql_password}",
+        command     => "/usr/bin/mysql -uroot -e \"set wsrep_on='off'; delete from mysql.user where user=''; grant all on *.* to '${mysql_user}'@'%' identified by '${mysql_password}';\"",
+        require     => Service["mysql-galera"],
+        subscribe   => Service["mysql-galera"],
+        refreshonly => true,
     }
 }
