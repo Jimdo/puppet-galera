0e88bf4dbd91af45169a9aa802b60f7c5f31610f
danehans <danehans@cisco.com>
2012-08-29 21:11:57 +0000

    added required galera pkg dependencies

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,12 +3,11 @@ class galera($cluster_name, $master_ip = false) {
 
     $mysql_user           = "wsrep_sst"
     $mysql_password       = "password"
-#    $mysql_root_password  = "ubuntu"
 
     service { "mysql-galera" :
         name        => "mysql",
         ensure      => "running",
-        require     => [Package["mysql-server-wsrep","galera"],File["/etc/mysql/conf.d/wsrep.cnf","/etc/mysql/my.cnf"]],
+        require     => [Package["mysql-server-wsrep","galera","mysql-client-5.5","libaio1","libssl0.9.8"],File["/etc/mysql/conf.d/wsrep.cnf","/etc/mysql/my.cnf"]],
         hasrestart  => true,
 #       hasstatus   => true, // http://projects.puppetlabs.com/issues/5610
     }
@@ -17,24 +16,32 @@ class galera($cluster_name, $master_ip = false) {
         ensure      => present,
     }
 
+    package { "libaio1" :
+        ensure      => present,
+    }
+
+    package { "libssl0.9.8" :
+        ensure      => present,
+    }
+
     package { "mysql-server-wsrep" :
         ensure      => present,
         provider    => "dpkg",
         source      => "/tmp/mysql-server-wsrep-5.5.23-23.6-amd64.deb",
-        require     => [Exec["download-wsrep"],Package["mysql-client-5.5"]],
-    }
-
-    exec { "download-wsrep" :
-        command     => "wget -O /tmp/mysql-server-wsrep-5.5.23-23.6-amd64.deb http://launchpad.net/codership-mysql/5.5/5.5.23-23.6/+download/mysql-server-wsrep-5.5.23-23.6-amd64.deb",
-        path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-	creates     => "/tmp/mysql-server-wsrep-5.5.23-23.6-amd64.deb",
+        require     => [Exec["download-wsrep"],Package["mysql-client-5.5","libaio1","libssl0.9.8"]],
     }
 
     package { "galera" :
         ensure      => present,
         provider    => "dpkg",
         source      => "/tmp/galera-23.2.1-amd64.deb",
-        require     => [Exec["download-galera"],Package["mysql-client-5.5"]],
+        require     => [Exec["download-galera"],Package["mysql-client-5.5","libaio1","libssl0.9.8"]],
+    }
+
+    exec { "download-wsrep" :
+        command     => "wget -O /tmp/mysql-server-wsrep-5.5.23-23.6-amd64.deb http://launchpad.net/codership-mysql/5.5/5.5.23-23.6/+download/mysql-server-wsrep-5.5.23-23.6-amd64.deb",
+        path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+	creates     => "/tmp/mysql-server-wsrep-5.5.23-23.6-amd64.deb",
     }
 
     exec { "download-galera" :
@@ -46,19 +53,13 @@ class galera($cluster_name, $master_ip = false) {
     file { "/etc/mysql/conf.d/wsrep.cnf" :
         ensure      => present,
         content     => template("galera/wsrep.cnf.erb"),
-        require     => [Exec["force-pkg-install"],Package["mysql-server-wsrep","galera"]],
+        require     => Package["mysql-server-wsrep","galera"],
     }
 
     file { "/etc/mysql/my.cnf" :
         ensure      => present,
         content     => template("galera/my.cnf.erb"),
-        require     => [Exec["force-pkg-install"],Package["mysql-server-wsrep","galera"]],
-    }
-
-    exec { "force-pkg-install" :
-        command     => "apt-get -f install -y",
-        path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-	#require     => [Package["mysql-server-wsrep","galera"]],
+        require     => Package["mysql-server-wsrep","galera"],
     }
 
     exec { "set-mysql-password" :
